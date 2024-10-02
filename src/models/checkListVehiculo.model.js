329006157562186c91f78f5506cebe3c82ba41f4
Daniel Rangel <danizuru99@gmail.com>
2024-10-02 07:10:08 -0500

    api vehicle changes

--- src/models/checkListVehiculo.model.js
+++ src/models/checkListVehiculo.model.js
@@ -10,271 +10,73 @@ const checklistVehiculoSchema = new Schema({
     horaInspeccion: { type: String, required: true },
     kmInicial: { type: Number, required: true },
     kmFinal: { type: Number, required: true },
-    TipoConsumo: { 
-        type: String, 
-        enum: ['Gas', 'Gasolina', 'Electrico'],
-        required: true
-    },
-    AsientoTrasero: { 
-        type: Boolean, 
-        required: true
-    },
+    TipoConsumo: { type: String, enum: ['Gas', 'Gasolina', 'Electrico'], required: true },
+    AsientoTrasero: { type: Boolean, required: true },
     sistemaLuces: {
-        luzDelanteraAlta: {
-            type: String,
-            enum: ['B', 'M', 'NA'],
-            required: true
-        },
-        luzDelanteraBaja: {
-            type: String,
-            enum: ['B', 'M', 'NA'],
-            required: true
-        },
-        lucesEmergencia: {
-            type: String,
-            enum: ['B', 'M', 'NA'],
-            required: true
-        },
-        lucesDireccionalesDelanteras: {
-            type: String,
-            enum: ['B', 'M', 'NA'],
-            required: true
-        },
-        lucesDireccionalesPosteriores: {
-            type: String,
-            enum: ['B', 'M', 'NA'],
-            required: true
-        },
-        luzReversa: {
-            type: String,
-            enum: ['B', 'M', 'NA'],
-            required: true
-        },
-        lucesdeFrenoPosterior: { 
-            type: String, 
-            enum: ['B', 'M', 'NA'], 
-            required: true 
-        }
+        luzDelanteraAlta: { type: Boolean, required: true },
+        luzDelanteraBaja: { type: Boolean, required: true },
+        lucesEmergencia: { type: Boolean, required: true },
+        lucesDireccionalesDelanteras: { type: Boolean, required: true },
+        lucesDireccionalesPosteriores: { type: Boolean, required: true },
+        luzReversa: { type: Boolean, required: true },
+        lucesdeFrenoPosterior: { type: Boolean, required: true }
     },
     parteExterna: {
-        parabrisasDelantero: {
-            type: String,
-            enum: ['B', 'M', 'NA'],
-            required: true
-        },
-        parabrisasPosterior: {
-            type: String,
-            enum: ['B', 'M', 'NA'],
-            required: true
-        },
-        limpiaparabrisas: {
-            type: String,
-            enum: ['B', 'M', 'NA'],
-            required: true
-        },
-        sapitos: {
-            type: String,
-            enum: ['B', 'M', 'NA'],
-            required: true
-        },
-        niveldeagua: {
-            type: String,
-            enum: ['B', 'M', 'NA'],
-            required: true
-        },
-        niveldeAceite: {
-            type: String,
-            enum: ['B', 'M', 'NA'],
-            required: true
-        },
-        perdidadeFluidosPavimento: {
-            type: String,
-            enum: ['B', 'M', 'NA'],
-            required: true
-        }
+        parabrisasDelantero: { type: Boolean, required: true },
+        parabrisasPosterior: { type: Boolean, required: true },
+        limpiaparabrisas: { type: Boolean, required: true },
+        sapitos: { type: Boolean, required: true },
+        niveldeagua: { type: Boolean, required: true },
+        niveldeAceite: { type: Boolean, required: true },
+        perdidadeFluidosPavimento: { type: Boolean, required: true }
     },
     parteInterna: {
-        tableroEstadoIndicadores: {
-            type: String,
-            enum: ['B', 'M', 'NA'],
-            required: true
-        },
-        frenoMano: { 
-            type: String, 
-            enum: ['B', 'M', 'NA'], 
-            required: true 
-        },
-        frenoServicio: {
-            type: String,
-            enum: ['B', 'M', 'NA'],
-            required: true
-        },
-        cinturonChofer: {
-            type: String,
-            enum: ['B', 'M', 'NA'],
-            required: true
-        },
-        cinturonCopiloto: {
-            type: String,
-            enum: ['B', 'M', 'NA'],
-        },
-        cinturonPasajeros: {
-            type: String,
-            enum: ['B', 'M', 'NA'],
-            required: true
-        },
-        espejoRetrovisor: {
-            type: String,
-            enum: ['B', 'M', 'NA'],
-            required: true
-        },
-        ordenLimpieza: {
-            type: String,
-            enum: ['B', 'M', 'NA'],
-            required: true
-        },
-        direccion: { 
-            type: String, 
-            enum: ['B', 'M', 'NA'], 
-            required: true 
-        },
-        suspension: {
-            type: String,
-            enum: ['B', 'M', 'NA'],
-            required: true
-        }
+        tableroEstadoIndicadores: { type: Boolean, required: true },
+        frenoMano: { type: Boolean, required: true },
+        frenoServicio: { type: Boolean, required: true },
+        cinturonChofer: { type: Boolean, required: true },
+        cinturonCopiloto: { type: Boolean },
+        cinturonPasajeros: { type: Boolean, required: true },
+        espejoRetrovisor: { type: Boolean, required: true },
+        ordenLimpieza: { type: Boolean, required: true },
+        direccion: { type: Boolean, required: true },
+        suspension: { type: Boolean, required: true }
     },
     estadoCubiertas: {
-        llantaDelanteraDerecha: {
-            type: String,
-            enum: ['B', 'M', 'NA'],
-            required: true
-        },
-        llantaDelanteraIzquierda: {
-            type: String,
-            enum: ['B', 'M', 'NA'],
-            required: true
-        },
-        llantaPosteriorDerecha: {
-            type: String,
-            enum: ['B', 'M', 'NA'],
-            required: true
-        },
-        llantaPosteriorIzquierda: {
-            type: String,
-            enum: ['B', 'M', 'NA'],
-            required: true
-        },
-        llantaRepuesto: {
-            type: String,
-            enum: ['B', 'M', 'NA'],
-            required: true
-        },
-        presionNeumaticos: {
-            type: String,
-            enum: ['B', 'M', 'NA'],
-            required: true
-        }
+        llantaDelanteraDerecha: { type: Boolean, required: true },
+        llantaDelanteraIzquierda: { type: Boolean, required: true },
+        llantaPosteriorDerecha: { type: Boolean, required: true },
+        llantaPosteriorIzquierda: { type: Boolean, required: true },
+        llantaRepuesto: { type: Boolean, required: true },
+        presionNeumaticos: { type: Boolean, required: true }
     },
     accesoriosSeguridad: {
-        conosSeguridad: {
-            type: String,
-            enum: ['B', 'M', 'NA'],
-            required: true
-        },
-        extintor: { 
-            type: String, 
-            enum: ['B', 'M', 'NA'], 
-            required: true 
-        },
-        alarmaRetroceso: {
-            type: String,
-            enum: ['B', 'M', 'NA'],
-            required: true
-        },
-        bosina: {
-            type: String,
-            enum: ['B', 'M', 'NA'],
-            required: true
-        },
-
-        
-        
-        radioComunicacion: {
-            type: String,
-            enum: ['B', 'M', 'NA'],
-            required: true
-        },
-        linternaMano: {
-            type: String,
-            enum: ['B', 'M', 'NA'],
-            required: true
-        }
+        conosSeguridad: { type: Boolean, required: true },
+        extintor: { type: Boolean, required: true },
+        alarmaRetroceso: { type: Boolean, required: true },
+        bosina: { type: Boolean, required: true },
+        radioComunicacion: { type: Boolean, required: true },
+        linternaMano: { type: Boolean, required: true }
     },
-    tapasyOtros:{
-        
-        tapaTanqueGasolina: {
-            type: String,
-            enum: ['B', 'M', 'NA'],
-            required: true
-        },
-        
-        gataHidraulica: {
-            type: String,
-            enum: ['B', 'M', 'NA'],
-            required: true
-        },
-        herramientasPalancaRuedas: {
-            type: String,
-            enum: ['B', 'M', 'NA'],
-            required: true
-        },
-        cablePasaCorriente: { 
-            type: String, 
-            enum: ['B', 'M', 'NA'], 
-            required: true 
-        },
-        eslingasZunchos: { 
-            type: String, 
-            enum: ['B', 'M', 'NA'], 
-            required: true 
-        },
-        OtrasHerramientas: { 
-            type: String, 
-            enum: ['B', 'M', 'NA'], 
-            required: true 
-        },
+    tapasyOtros: {
+        tapaTanqueGasolina: { type: Boolean, required: true },
+        gataHidraulica: { type: Boolean, required: true },
+        herramientasPalancaRuedas: { type: Boolean, required: true },
+        cablePasaCorriente: { type: Boolean, required: true },
+        eslingasZunchos: { type: Boolean, required: true },
+        OtrasHerramientas: { type: Boolean, required: true }
     },
-
     documentacion: {
-        tarjetaPropiedad: {
-            type: String,
-            enum: ['B', 'M', 'NA'],
-            required: true
-        },
-        PapelesSeguro: { 
-            type: String, 
-            enum: ['B', 'M', 'NA'], 
-            required: true 
-        },
-        autorizacionTransito: {
-            type: String,
-            enum: ['B', 'M', 'NA'],
-            required: true
-        },
-        licenciaConducir: {
-            type: String,
-            enum: ['B', 'M', 'NA'],
-            required: true
-        },
-        tarjetaGNC: { 
-            type: String, 
-            enum: ['B', 'M', 'NA'],  
-        },
+        tarjetaPropiedad: { type: Boolean, required: true },
+        PapelesSeguro: { type: Boolean, required: true },
+        autorizacionTransito: { type: Boolean, required: true },
+        licenciaConducir: { type: Boolean, required: true },
+        tarjetaGNC: { type: Boolean }
     },
     observaciones: { type: String, default: '' },
     choquesYRaspaduras: { type: String, default: '' },
     afirmacion: { type: Boolean, default: false }
-}, { timestamps: true })
+}, { timestamps: true });
+
 
 export default model('ChecklistVehiculo', checklistVehiculoSchema)
