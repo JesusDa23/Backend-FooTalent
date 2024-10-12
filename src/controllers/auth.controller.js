386c03c7ff7f90b07c04412f22c312264ac40c17
JesarRL <ljesar01@gmail.com>
2024-10-12 01:38:05 -0500

    asdasd

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -32,9 +32,9 @@ Auth.forgotPasswordForEmail = async (req, res) => {
 
 // aca puse los datos adicionales que se estan pidiendo 
 Auth.register = async (req, res) => {
-    const { dni, name, email, phone, password, licencia, address, isFirstLogin, rol } = req.body
+    const { dni, name, email, phone, address, password, licencia, type_licence, isFirstLogin, rol } = req.body
     try {
-        const user = await AuthService.register(dni, name, email, phone, password, licencia, address, isFirstLogin, rol)
+        const user = await AuthService.register(dni, name, email, phone, address, password, licencia, type_licence, isFirstLogin, rol)
 
         NotificationController.sendEmail(
             email,
@@ -46,24 +46,6 @@ Auth.register = async (req, res) => {
         res.status(500).json({ error: error.message })
     }
 }
-// Este es el controlador original antes de hacerle cambios, lo deje comentado por si acaso :v
-// Auth.register = async (req, res) => {
-//     const { dni, name, email, phone, password, rol } = req.body
-//     try {
-//         const user = await AuthService.register(dni, name, email, phone, password, rol)
-
-//         NotificationController.sendEmail(
-//             email,
-//             'Bienvenido a Footalent',
-//             'Gracias por registrarte en Footalent'
-//         )
-//         res.status(201).json(user)
-//     } catch (error) {
-//         res.status(500).json({ error: error.message })
-//     }
-// }
-
-
 
 Auth.profile = async (req, res) => {
     console.log(req)
@@ -75,12 +57,8 @@ Auth.profile = async (req, res) => {
     }
 }
 
-Auth.getUsers = async (req, res) => {
+Auth.findUser = async (req, res) => {
     try {
-        const allUsers = await User.find()
-        res.json({
-            data: allUsers
-        })
         const { id } = req.params
 
         if (id) {
@@ -96,13 +74,12 @@ Auth.getUsers = async (req, res) => {
             res.status(400).json({ message: "no Id enviado" });
         }
     } catch (error) {
-        res.json(error)
+        res.status(500).json({ error: error.message });
     }
 }
 
-Auth.getUser = async (req, res) => {
+Auth.getUsers = async (req, res) => {
     try {
-        const dataUser = await User.findOne({ email: req.params.email })
         const users = await User.find({});
 
         if (!users) {
@@ -110,10 +87,10 @@ Auth.getUser = async (req, res) => {
         }
 
         res.json({
-            dataUser
-        })
+            data: users
+        });
     } catch (error) {
-        res.json(error)
+        res.status(500).json({ error: error.message });
     }
 };
 
@@ -141,52 +118,20 @@ Auth.deleteUser = async (req, res) => {
     }
 };
 
-Auth.forgotPassword = async (req, res) => {
-    const { dni } = req.params
-    const { oldPassword, newPassword, forEmail } = req.body
-
-    try {
-        const user = await AuthService.forgotPassword(dni, oldPassword, newPassword, forEmail)
-        res.status(200).json(user)
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({ error: error.message })
-    }
-}
-
-Auth.updateFirstLogin = async (req, res) => {
-    const { dni } = req.params;
-    const { isFirstLogin } = req.body;
-
-    try {
-        const user = await User.findOneAndUpdate({ dni }, { isFirstLogin }, { new: true });
-
-        if (!user) {
-            return res.status(404).json({ error: 'Usuario no encontrado' });
-        }
-
-        res.status(200).json({
-            message: 'isFirstLogin actualizado exitosamente',
-            user
-        });
-    } catch (error) {
-        console.log('error:', error)
-        res.status(500).json({ error: 'Error al actualizar isFirstLogin' });
-    }
-};
-
-
-Auth.forgotPasswordForEmail = async (req, res) => {
-    const { email } = req.params
 Auth.updateUser = async (req, res) => {
     const { id } = req.params;
     const { name, dni, phone, email, address, licence, type_licence, expiration_licence } = req.body; // Extract data from request body
 
     try {
-        const tokenUser = await AuthService.forgotPasswordForEmail(email)
-        res.status(200).json(tokenUser)
+        const updatedUser = await User.findByIdAndUpdate(id, { name, dni, phone, email, address, licence, type_licence, expiration_licence }, { new: true });
+
+        if (!updatedUser) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        res.status(200).json(updatedUser);
     } catch (error) {
-        res.status(500).json({ error: error.message })
+        res.status(400).json({ message: 'Error updating user', error: error.message });
     }
 }
 
