4926112b93b8be8a7c0542aa300b367c361a6a79
Luca-V <lucav558@gmail.com>
2024-10-22 12:02:51 -0300

    swagger system deleted

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import app from './app.js'
 import { connectToDatabase } from './db.js'
-import { swaggerDocs } from './docs.js'
 import executeRegister from './utils/executeRegister.js'
 
 await connectToDatabase()
 
 app.listen(process.env.AppPort, (res, error) => {
     console.log('Server running at http://localhost:' + process.env.AppPort)
-    swaggerDocs(app)
     // executeRegister()
 })
